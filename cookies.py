2c8ad7f788a047637df27b1462a6f706214165d0
Codewithdark <144595403+codewithdark-git@users.noreply.github.com>
2024-03-14 17:36:23 +0500

    Create cookies.py

---
+++ cookies.py
@@ -0,0 +1,9 @@
+# import
+# # Set cookies for ".bing.com"
+# set_cookies(".bing.com", {"_U": "ipv6"})
+#
+# # Set cookies for "chat.openai.com"
+# set_cookies("chat.openai.com", {"access_token": "cf_clearance"})
+#
+# # Set cookies for ".google.com"
+# set_cookies(".google.com", {"__Secure-1PSID": "__Secure-ENID"})
